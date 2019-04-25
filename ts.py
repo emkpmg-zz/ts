d08a3296db783644347917a9a0e12970b4444fa0
Deborah Akuoko <deborah.akuoko@postgrad.manchester.ac.uk>
2019-04-25 17:49:45 +0100

    #Data transformation #Fixing missing values

--- ts.py
+++ ts.py
@@ -11,7 +11,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 #matplotlib inline
 
-# 01 Data exploration: Load training or test data with pd.read_csv, print sample to see how data looks
+# 01 DATA LOADING, EXPLORATION AND VISUALIZATION
+#Load training or test data with pd.read_csv, print sample to see how data looks
 trainingData = pd.read_csv('train.csv')
 testData = pd.read_csv('test.csv')
 
@@ -52,4 +53,46 @@ ageSurvive.set( xlim=( 0 , trainingData[ 'Age' ].max() ) )
 ageSurvive.add_legend()
 
 # survival for of males and females according to age. Significant difference
-# Difference in curves is a sign that age is a relevant variable for predicting survival(target) 
+# Difference in curves is a sign that age is a relevant variable for predicting survival(target)
+
+
+
+# 02 DATA TRANSFORMATION AND MISSING VALUES
+
+#Categorical variables (Embarked, Pclass and Sex)
+#Necessary to convert to numeric value to #ensure easy data processing by model algorithms.
+#This is done by creating dummy variables of numeric values for every categorical variable
+
+#combine both training and test datasets
+bothDatasets = trainingData.append( testData , ignore_index = True )
+
+#select 891 rows out of bothDatasets (891 is the count of the bigger set - training set)
+#selectedData = bothDatasets[ :891 ]
+
+# Transform var Sex into 0 and 1 (Both for Training and Test Datasets). This is boolean, thus we have to hypothesize that gender is for
+# instance female. So if  true, Sex = 1, else Sex = 0
+sex = pd.Series( np.where( bothDatasets.Sex == 'female' , 1 , 0 ) , name = 'Sex' )
+#view top rows for var Sex
+sex.head()
+
+# Create a new variable for every unique value of Embarked
+embarked = pd.get_dummies( bothDatasets.Embarked , prefix='Embarked' )
+#view top rows for var embarked
+embarked.head()
+
+#Missing Variables
+#Most alghorims don't expect null values. Thus, missing values for variables can be polulated with an average of that variable's values as observed in the training set.
+
+# Create an empty dataset to hold the populated missing values for Age and Fare
+imputed = pd.DataFrame()
+
+# Substitute missing Age values with mean Age
+imputed[ 'Age' ] = bothDatasets.Age.fillna( bothDatasets.Age.mean() )
+
+# Substitute missing Fare values with mean Fare
+imputed[ 'Fare' ] = bothDatasets.Fare.fillna( bothDatasets.Fare.mean() )
+
+#compare original data sample to imputed sample to observe populated missing values
+imputed.sample(20)
+bothDatasets.sample(20)
+
